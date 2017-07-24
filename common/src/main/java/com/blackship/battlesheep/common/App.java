eadb02093e64fadce714b3c7c641a4800089984f
Anna Gawda <a.m.gawda@gmail.com>
2017-07-24 17:39:20 +0200

    Add empty main to common in order to generate mvn site reports

---
+++ common/src/main/java/com/blackship/battlesheep/common/App.java
@@ -0,0 +1,11 @@
+package com.blackship.battlesheep.common;
+
+/**
+ * @author Anna Gawda
+ * @since 24.07.17
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
