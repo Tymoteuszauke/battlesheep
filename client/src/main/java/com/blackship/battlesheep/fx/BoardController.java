7a4e799236735939847ef981450cc67a6c140668
Mateusz Slabonski <mateusz_slabonski@epam.com>
2017-07-26 14:50:55 +0200

    BoardController fix

--- client/src/main/java/com/blackship/battlesheep/fx/BoardController.java
+++ client/src/main/java/com/blackship/battlesheep/fx/BoardController.java
@@ -2,7 +2,9 @@ package com.blackship.battlesheep.fx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -29,6 +31,23 @@ public class BoardController {
         String moveValue = positionTextField.getText();
         loggerInfoTextArea.appendText(moveValue + "\n");
         positionTextField.clear();
+
+        int position = Integer.parseInt(moveValue) - 1;
+        int[] positionCoordinates = calculateOneDimensionalPosition(position);
+
+        Label label = new Label("X");
+        label.setStyle("-fx-font-size: 25px;");
+
+        playerGridPane.add(label, positionCoordinates[0], positionCoordinates[1]);
+        GridPane.setHalignment(label, HPos.CENTER);
+
+    }
+
+    private int[] calculateOneDimensionalPosition(int position) {
+        int x = position % 10;
+        int y = position / 10;
+        return new int[] {x, y};
+
     }
 
 }
