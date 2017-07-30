89888aaa221b707688b2cbed1bd612e75b91c748
Mateusz Słaboński <mateusz.slabonski@gmail.com>
2017-07-31 00:05:07 +0200

    Added logger to common module, usage examples witin GUI package

--- client/src/main/java/com/blackship/battlesheep/fx/controllers/utils/PositionUtils.java
+++ client/src/main/java/com/blackship/battlesheep/fx/controllers/utils/PositionUtils.java
@@ -6,13 +6,13 @@ package com.blackship.battlesheep.fx.controllers.utils;
  */
 public class PositionUtils {
 
-    public static int calculateFromTwoDimensionalPosition(int columns, int row, int column) {
+    public static int calculateFromTwoDimensionalPosition(int columns, int column, int row) {
         return row * columns + column;
     }
 
     public static int[] calculateFromOneDimensionalPosition(int position, int boardSize) {
-        int y = position % boardSize;
-        int x = position / boardSize;
+        int x = position % boardSize;
+        int y = position / boardSize;
         return new int[] {x, y};
     }
 }
