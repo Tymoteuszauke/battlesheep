d52cc8089bd3bb78d046901a1ac7650d4e35797b
Mateusz Slabonski <mateusz_slabonski@epam.com>
2017-08-02 13:52:06 +0200

    README.md updated, player button board implemented with hardcoded player ships

--- common/src/main/java/com/blackship/battlesheep/utils/LogUtils.java
+++ common/src/main/java/com/blackship/battlesheep/utils/LogUtils.java
@@ -4,19 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
-<<<<<<< HEAD
- * Created by Mateusz on 30.07.2017.
-=======
+ *
  * @author Mateusz Słaboński
  * @since 30.07.2017
->>>>>>> dev_communication_milosz
  */
-public class LogUtils extends SecurityManager{
+public class LogUtils extends SecurityManager {
 
     public static Logger getLogger() {
         String className = new LogUtils().getClassName();
-        Logger logger = LoggerFactory.getLogger(className);
-        return logger;
+        return LoggerFactory.getLogger(className);
     }
 
     private String getClassName()
